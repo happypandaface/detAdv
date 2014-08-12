d24714ef5fe6c756bc6d96b92b477fbb2464415c
woodensplint <woodensplint@gmail.com>
2014-08-12 02:35:56 -0700

    adding diner scene

--- Test.java
+++ Test.java
@@ -16,6 +16,51 @@ public class Test
 	public void run()
 	{
 		Scanner inputReader = new Scanner(System.in);
+		System.out.println("The newspaper is dated March 15th 1987. The headline is something about another Hollywood overdose. Filth. There is some dirt under my fingernails.");
+		System.out.println("Continue... (press enter)");
+		inputReader.nextLine();
+		System.out.println("What can I get for you darling?");
+		System.out.println("1: Croissant \n2: Coffee");
+		String dinerchoice = inputReader.nextLine();
+		if (dinerchoice.equals("1")){
+			System.out.println("Gimme a Croissant. \n Coming right up!");
+		}
+		else
+		{
+			System.out.println("Just a coffee, thanks.\n Croissants always upset my stomach. \nThe waitress gives me a look I know all too well and turns away without another word. ");
+		}
+		System.out.println("Peering over my newspaper I notice a man is standing at the front counter.");
+		System.out.println("The bells on the door jingle as the only other patron comes in.");
+		System.out.println("Continue... (press enter)");
+		inputReader.nextLine();
+		System.out.println("There's some sort of commotion with the new patron");
+		System.out.println("Continue... (press enter)");
+		inputReader.nextLine();
+		System.out.println("Put the fucking money in the fucking bag!");
+		System.out.println("Continue... (press enter)");
+		inputReader.nextLine();
+		System.out.println("This whole city has gone to shit.");
+		System.out.println("1: confront \n2: Leave");
+		String fightchoice = inputReader.nextLine();
+		if (fightchoice.equals("1")){
+			System.out.println("This guy chose a bad day for crime.");
+			System.out.println("Continue... (press enter)");
+			inputReader.nextLine();
+			System.out.println("Hurry it up already!");
+		}
+		else
+		{
+			System.out.println("This lowlife isn't worth my effort.");
+			System.out.println("Continue... (press enter)");
+			inputReader.nextLine();
+			System.out.println("I sneak out the back door");
+			System.out.println("Continue... (press enter)");
+			inputReader.nextLine();
+		}
+		
+		
+		
+		/*Scanner inputReader = new Scanner(System.in);
 		System.out.println("Enter your name:");
 		String name = inputReader.nextLine();
 		System.out.println("Welcome, "+name+", to the world of pokemon (press enter)!");
@@ -40,7 +85,7 @@ public class Test
 			JLabel label = new JLabel(new ImageIcon("game.png"));
 			frame.getContentPane().add(label);
 		}
-		
+		*/
 		
 		
 		System.out.println("hit enter!");
