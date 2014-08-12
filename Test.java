3107b32f8cf1525295fb7ba5aa30813f239fd845
Scott Griffy <scottgriffy@gmail.com>
2014-08-12 00:36:14 -0700

    multiple frames

--- Test.java
+++ Test.java
@@ -12,17 +12,28 @@ public class Test
 		String name = inputReader.nextLine();
 		System.out.println("Welcome, "+name+", to the world of pokemon (press enter)!");
 		
-		JFrame frame = new JFrame();
-		frame.setSize ( 100, 100 );
-		frame.setTitle( "image of game" );
-		frame.setVisible( true );
+		{
+			JFrame frame = new JFrame();
+			frame.setSize ( 100, 100 );
+			frame.setTitle( "image of game" );
+			frame.setVisible( true );
+			Rectangle r = new Rectangle(100, 100, 200, 200);
+			frame.setBounds(r);
+			JLabel label = new JLabel(new ImageIcon("game.png"));
+			frame.getContentPane().add(label);
+		}
+		{
+			JFrame frame = new JFrame();
+			frame.setSize ( 100, 100 );
+			frame.setTitle( "image of game" );
+			frame.setVisible( true );
+			Rectangle r = new Rectangle(200, 100, 200, 200);
+			frame.setBounds(r);
+			JLabel label = new JLabel(new ImageIcon("game.png"));
+			frame.getContentPane().add(label);
+		}
 		
-        JLabel label = new JLabel(new ImageIcon("game.png"));
-		frame.getContentPane().add(label);
 		
-		
-		Rectangle r = new Rectangle(100, 100, 200, 200);
-		frame.setBounds(r);
 		
 		System.out.println("hit enter!");
 		inputReader.nextLine();
