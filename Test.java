0b54055d5a62986d814ba41ecb1a897d98782c1d
Scott Griffy <scottgriffy@gmail.com>
2014-08-12 00:21:42 -0700

    first test

---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter your name:");
+		Scanner inputReader = new Scanner(System.in);
+		String name = inputReader.nextLine();
+		System.out.println("Welcome, "+name+", to the world of pokemon (press enter)!");
+		
+		JFrame frame = new JFrame();
+		frame.setSize ( 100, 100 );
+		frame.setTitle( "image of game" );
+		frame.setVisible( true );
+		
+		Rectangle r = new Rectangle(100, 100, 200, 200);
+		frame.setBounds(r);
+		
+		System.out.println("hit enter!");
+		inputReader.nextLine();
+		System.exit(0);
+	}
+}
