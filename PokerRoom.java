af5b6bb7e88dbc40ad52d9f8ac1c9640e807ade2
Scott Griffy <scottgriffy@gmail.com>
2014-08-12 18:26:56 -0700

    more betting logic and random bets

--- PokerRoom.java
+++ PokerRoom.java
@@ -266,19 +266,186 @@ class Pot
 	long totalMoney;
 	long highestBet;
 	List<Integer> players;
+	Map<Integer, Integer> playerBet;
+	List<Integer> foldedPlayers;
 	
 	public Pot()
 	{
-		
+		players = new ArrayList<Integer>();
+		foldedPlayers = new ArrayList<Integer>();
+		playerBet = new HashMap<Integer, Integer>();
 	}
 	public void addPlayer(int player)
 	{
 		players.add(player);
 	}
-	public void raise(int gold)
+	public void removePlayer(int player)
 	{
+		foldedPlayers.add(player);
+	}
+	public void removePlayerNow(int player)
+	{
+		for (int i = players.size()-1; i >= 0; --i)
+			if (players.get(i) == player)
+				players.remove(i);
+	}
+	public void removeFoldedPlayers()
+	{
+		for (int i = foldedPlayers.size()-1; i >= 0; --i)
+		{
+			removePlayerNow(foldedPlayers.get(i));
+			foldedPlayers.remove(i);
+		}
+
+	}
+	public int raise(int player, int gold)
+	{
+		int alreadyBet = 0;
+		if (playerBet.get(player) != null)
+			alreadyBet = playerBet.get(player);
+		int makeUp = 0;
+		if (alreadyBet < highestBet)
+			makeUp += highestBet-alreadyBet;
+		int thisBet = makeUp+gold;
+		playerBet.put(player, alreadyBet+thisBet);
 		highestBet += gold;
-		totalMoney += highestBet;
+		totalMoney += makeUp+gold;
+		return thisBet;
+	}
+	
+	public void doBetting(GameVars gv)
+	{
+		playerBet.clear();
+		boolean ripe = false;
+		while (!ripe)
+		{
+			for (int i = 0; i < players.size(); ++i)
+			{
+				int playerNum = players.get(i);
+				int choice = 0;
+				int betMoney = 0;
+				if (playerNum == 0)
+				{
+					ChoiceMenu handAction = new ChoiceMenu();
+					System.out.println("What do I do?");
+					handAction.addOption("raise");
+					handAction.addOption("call");
+					handAction.addOption("fold");
+					handAction.execute(gv);
+					choice = handAction.getChoice();
+					if (choice == 1)
+					{
+						System.out.print("How much do you wanna raise: ");
+						betMoney = DetUtil.getGTZeroInt();
+						System.out.println("");
+					}
+				}else
+				{
+					choice = (int)Math.floor(Math.random()*3f)+1;
+					betMoney = (int)Math.floor(Math.random()*300f)+1;
+				}
+				if (choice == 1)
+				{
+					// check player's moneys to make more pots if necessary
+					raise(playerNum, betMoney);
+					System.out.println("player "+playerNum+" raises "+betMoney);
+				}else 
+				if (choice == 2)
+				{
+					raise(playerNum, 0);
+					System.out.println("player "+playerNum+" calls");
+				}else
+				if (choice == 3)
+				{
+					removePlayer(playerNum);
+					System.out.println("player "+playerNum+" folds");
+				}
+			}
+			removeFoldedPlayers();
+			// check if pot's ripe!
+			ripe = true;
+			for (int i = 0; i < players.size(); ++i)
+			{
+				int playerNum = players.get(i);
+				System.out.println(playerNum+", "+playerBet.get(playerNum)+", "+highestBet);
+				if (playerBet.get(playerNum) != highestBet)
+					ripe = false;
+			}
+		}
+		System.out.println("Pot's ripe! "+totalMoney+" gold");
+		DetUtil.doContinue();
+	}
+	
+	public void findWinners(PokerRoom pr)
+	{
+		long bestPlayerHigh = 0;
+		Hand bestOverallHand = null;
+		List<Integer> winners = new ArrayList<Integer>();
+		for (int i = 0; i < players.size(); ++i)
+		{
+			long currPlayerVal = 0;
+			Hand bestHand = null;
+			// every combination of hands
+			for (int c1 = 0; c1 < 7; ++c1)
+				for (int c2 = 0; c2 < 7; ++c2)
+					for (int c3 = 0; c3 < 7; ++c3)
+						for (int c4 = 0; c4 < 7; ++c4)
+							for (int c5 = 0; c5 < 7; ++c5)
+							{
+								if (c1 != c2 &&
+									c1 != c3 &&
+									c1 != c4 &&
+									c1 != c5 &&
+									c2 != c3 &&
+									c2 != c4 &&
+									c2 != c5 &&
+									c3 != c4 &&
+									c3 != c5 &&
+									c4 != c5)
+								{
+									Hand h = new Hand();
+									h.addCard(pr.getCardForPlayer(i, c1));
+									h.addCard(pr.getCardForPlayer(i, c2));
+									h.addCard(pr.getCardForPlayer(i, c3));
+									h.addCard(pr.getCardForPlayer(i, c4));
+									h.addCard(pr.getCardForPlayer(i, c5));
+									long currVal = h.getVal();
+									if (currVal > currPlayerVal)
+									{
+										bestHand = h;
+										currPlayerVal = currVal;
+									}
+								}
+							}
+			if (currPlayerVal > bestPlayerHigh)
+			{
+				winners.clear();
+				bestPlayerHigh = currPlayerVal;
+				bestOverallHand = bestHand;
+				winners.add(i);
+			}else if (currPlayerVal == bestPlayerHigh)
+			{
+				winners.add(i);
+			}
+		}
+		String winString = "";
+		if (winners.size() == 1)
+		{
+			winString += "player "+winners.get(0);
+		}else
+		{
+			winString += "players ";
+			for (int i = 0; i < winners.size(); ++i)
+			{
+				winString += (winners.get(i)+1);
+				if (i == winners.size()-2)
+					winString += " and ";
+				else if (i != winners.size()-1)
+					winString += ", ";
+			}
+		}
+		System.out.println(winString+" wins, best hand: "+bestOverallHand.getDescription());
+		DetUtil.doContinue();
 	}
 }
 
@@ -321,112 +488,28 @@ public class PokerRoom extends Location
 			DetUtil.doContinue();
 			for (int i = 0; i < num_hands; ++i)
 			{
+				pot.addPlayer(i);
 				Hand h = new Hand().setRandom(hands);
 				hands.add(h);
-				System.out.println("player "+i+" has "+h.getDescription());
-				DetUtil.doContinue();
-			}
-			ChoiceMenu handAction = new ChoiceMenu();
-			handAction.addOption("raise");
-			handAction.addOption("call");
-			handAction.addOption("fold");
-			handAction.execute(gv);
-			for (int i = 0; i < pot.players.size(); ++i)
-			{
-				if (handAction.getChoice() == 1)
+				if (i == 0)
 				{
-					System.out.print("How much do you wanna raise: ");
-					int betMoney = DetUtil.getGTZeroInt();
-					System.out.println("");
-					pot.raise(betMoney);
-				}else 
-				if (handAction.getChoice() == 2)
-				{
-					pot.raise(0);
-				}else
-				if (handAction.getChoice() == 3)
-				{
-					
+					System.out.println("player "+i+" has "+h.getDescription());
+					DetUtil.doContinue();
 				}
 			}
-			System.out.println("Pot's ripe! "+pot.totalMoney+" gold");
-			DetUtil.doContinue();
+			pot.doBetting(gv);
 			hands.add(new Hand(3).setRandom(hands)); // flop
 			hands.add(new Hand(1).setRandom(hands)); // turn
 			hands.add(new Hand(1).setRandom(hands)); // river
 			System.out.println("the flop is "+getFlop().getDescription());
+			pot.doBetting(gv);
 			System.out.println("the turn is the "+getTurn().getName());
+			pot.doBetting(gv);
 			System.out.println("and the river is the "+getRiver().getName());
+			pot.doBetting(gv);
 			DetUtil.doContinue();
 			
-			long bestPlayerHigh = 0;
-			Hand bestOverallHand = null;
-			List<Integer> winners = new ArrayList<Integer>();
-			for (int i = 0; i < num_hands; ++i)
-			{
-				long currPlayerVal = 0;
-				Hand bestHand = null;
-				// every combination of hands
-				for (int c1 = 0; c1 < 7; ++c1)
-					for (int c2 = 0; c2 < 7; ++c2)
-						for (int c3 = 0; c3 < 7; ++c3)
-							for (int c4 = 0; c4 < 7; ++c4)
-								for (int c5 = 0; c5 < 7; ++c5)
-								{
-									if (c1 != c2 &&
-										c1 != c3 &&
-										c1 != c4 &&
-										c1 != c5 &&
-										c2 != c3 &&
-										c2 != c4 &&
-										c2 != c5 &&
-										c3 != c4 &&
-										c3 != c5 &&
-										c4 != c5)
-									{
-										Hand h = new Hand();
-										h.addCard(getCardForPlayer(i, c1));
-										h.addCard(getCardForPlayer(i, c2));
-										h.addCard(getCardForPlayer(i, c3));
-										h.addCard(getCardForPlayer(i, c4));
-										h.addCard(getCardForPlayer(i, c5));
-										long currVal = h.getVal();
-										if (currVal > currPlayerVal)
-										{
-											bestHand = h;
-											currPlayerVal = currVal;
-										}
-									}
-								}
-				if (currPlayerVal > bestPlayerHigh)
-				{
-					winners.clear();
-					bestPlayerHigh = currPlayerVal;
-					bestOverallHand = bestHand;
-					winners.add(i);
-				}else if (currPlayerVal == bestPlayerHigh)
-				{
-					winners.add(i);
-				}
-			}
-			String winString = "";
-			if (winners.size() == 1)
-			{
-				winString += "player "+winners.get(0);
-			}else
-			{
-				winString += "players ";
-				for (int i = 0; i < winners.size(); ++i)
-				{
-					winString += (winners.get(i)+1);
-					if (i == winners.size()-2)
-						winString += " and ";
-					else if (i != winners.size()-1)
-						winString += ", ";
-				}
-			}
-			System.out.println(winString+" wins, best hand: "+bestOverallHand.getDescription());
-			DetUtil.doContinue();
+			pot.findWinners(this);
 			
 			run(da, gv);
 		}
